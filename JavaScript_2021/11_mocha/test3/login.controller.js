f77324878f164ae1d87efc81e75af7b998c39f00
Sujeong Ji <suji.sujeongji@gmail.com>
2021-05-31 20:47:53 +0900

    test3

---
+++ JavaScript_2021/11_mocha/test3/login.controller.js
@@ -0,0 +1,9 @@
+function LoginController() {
+    function isValidUserId(userList, user) {
+        return userList.indexOf(user) >= 0;
+    }
+    return {
+        isValidUserId
+    }
+}
+module.exports = LoginController();
