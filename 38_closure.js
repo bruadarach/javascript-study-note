447c7adc9e8e0ee957ef23fccf4e28041a7d4484
Sujeong Ji <suji.sujeongji@gmail.com>
2022-04-07 23:43:59 +0900

    38_closure.js

--- 38_closure.js
+++ 38_closure.js
@@ -94,3 +94,27 @@ console.log(num2); // 1
 </body>
 </html>
 */
+
+/* 
+
+생활코딩 arguments
+  : 함수에는 arguments라는 변수에 담긴 숨겨진 유사 배열이 있다. 
+  : 이 배열에는 함수를 호출할 때 입력한 인자가 담겨있다. 
+  : 아래 예제를 보자. 결과는 10이다.
+  
+*/
+function sum() {
+  var i,
+    _sum = 0;
+  for (i = 0; i < arguments.length; i++) {
+    document.write(i + " : " + arguments[i] + "<br />");
+    _sum += arguments[i];
+  }
+  return _sum;
+}
+document.write("result : " + sum(1, 2, 3, 4));
+// 0 : 1<br />
+// 1 : 2<br />
+// 2 : 3<br />
+// 3 : 4<br />
+// result : 10
